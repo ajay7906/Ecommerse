5fbeafa9c8e44b7a8d6584528f78b2028b731211
Ajay Kumar <aks969014@gmail.com>
2024-02-21 01:33:13 +0530

    complete the Product card and search bar

--- src/Page/home/Home.jsx
+++ src/Page/home/Home.jsx
@@ -2,12 +2,17 @@ import React, { useContext } from 'react'
 import Layout from '../../components/layout/Layout'
 import myContext from '../../context/data/myContext'
 import Navbar from '../../components/navbar/Navbar'
+import HeroSection from '../../components/heroSection/HeroSection'
+import Filter from '../../components/filter/Filter'
+import ProductCard from '../../components/productCard/ProductCard'
 
 function Home() {
  
   return (
     <Layout>
-     Home
+      <HeroSection/>
+      <Filter/>
+      <ProductCard/>
     </Layout>
   )
 }
